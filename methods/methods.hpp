f2b0218bed91fa79b0f6a15ec434ceff30e485e5
Mikhail Lozhnikov <lozhnikovma@gmail.com>
2024-04-25 12:02:25 +0300

    Added breadth first traversal algorithm.

--- methods/methods.hpp
+++ methods/methods.hpp
@@ -14,6 +14,7 @@ namespace graph {
 
 /* Сюда нужно вставить объявление серверной части алгоритма. */
 
+int BreadthFirstTraversalMethod(const nlohmann::json& input, nlohmann::json* output);
 
 
 /* Конец вставки. */
